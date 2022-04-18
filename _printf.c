0e07f2a8c5a37a418d8df0c3147240a21cc7a9d6
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-04-18 20:50:02 +0300

    A function taht produces output according to a format and handles conversion specifiers

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@
  * 	 Tibebe Solomon
  */
 
-#include "holberton.h"
+#include "main.h"
 
 void cleanup(va_list args, buffer_t *output);
 int run_printf(const char *format, va_list args, buffer_t *output);
